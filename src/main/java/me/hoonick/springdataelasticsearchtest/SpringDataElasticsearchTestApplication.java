f7eaaef2ebd2989afe7502c1adeb69223cdac8d7
hoonick <wecube.1@gmail.com>
2020-04-24 12:43:57 +0900

    first commit

---
+++ src/main/java/me/hoonick/springdataelasticsearchtest/SpringDataElasticsearchTestApplication.java
@@ -0,0 +1,13 @@
+package me.hoonick.springdataelasticsearchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticsearchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataElasticsearchTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hoonick/springdataelasticsearchtest/SpringDataElasticsearchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoonick.springdataelasticsearchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
